d40a84cb68b4191e7d18435f19b3446ccac2021a
David <josedago1163@gmail.com>
2023-12-07 20:52:59 -0500

    reordenamos todo con Cookiecutter

--- miami_ds/scripts/app.py
+++ miami_ds/scripts/app.py
@@ -1,9 +1,8 @@
 import io
-from flask import Response
+from flask import Response, Flask, render_template
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from ipywidgets import interact
-from flask import Flask
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -14,16 +13,18 @@ app = Flask(__name__)
 @app.route('/')
 def plot_png():
     zs = np.random.rand(100)
+    
+    fig = Figure()
+    output = io.BytesIO()
+    axis = fig.add_subplot(1, 1, 1)
     xs = np.random.rand(100)
-    @interact
-    def f(x = ["xs", "zs"]):
-        fig = Figure()
-        axis = fig.add_subplot(1, 1, 1)
+    ys = np.random.rand(100)
+    axis.plot(xs, ys)
+    FigureCanvas(fig).print_png(output)
+    #I want to save the the figure
+    with open('static/images/image.png', 'wb') as file:
+        file.write(output.getvalue())
 
-        ys = np.random.rand(100)
-        axis.plot(x, ys)
-        output = io.BytesIO()
-        FigureCanvas(fig).print_png(output)
-        return Response(output.getvalue(), mimetype='image/png')
+    return render_template('index.html', r=Response(output.getvalue(), content_type='image/png'))
 
 app.run(debug=True, port=5000, use_reloader=False)
